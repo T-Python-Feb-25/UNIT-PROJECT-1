4b95f8d0f1834045523ddf1910540ab84664b36d
rahaf fallatah <rahaf77553@gmail.com>
2025-02-19 12:23:42 +0300

    The idea of the project was changed, the old idea files were deleted, files for the new idea were created, and the main file was programmed

--- main.py
+++ main.py
@@ -3,32 +3,26 @@
 
 from art import text2art
 from colorama import Fore, Back, Style
+import emoji
 #استدعاء لملفات البرنامج
-'''
-import log_in_and_ctreate_an_acconunt
-import available_books 
-import favorite_books_list 
-import book_purchase_requeste 
-'''
+
 while True:
     print(Fore.MAGENTA)
-    print("welcome to bookswap App\nchose what do you want to do\n ")
+    print("welcome to travelmate")
+    print("\nWhat would you like to do today\n ")
+    print("1.See my previous Plans   2.Create a new plan\n3.Exit the program")
+    userChoice=input("\nEnter a number ")
 
-    try: 
-        print("1.See available books    2.Go to my favorite\n3.go to my requests      4.Search for a book\n5.Exit the program")
-        userChoice=input("\nEnter a number ")
-
-        if userChoice=="1":
-            print("your choice is num 1")
-        elif userChoice=="2":
-            print("your choice is num 2")
-        elif userChoice=="3":
-            print("your choice is num 3")
-        elif userChoice=="4":
-            print("your choice is num 4")
-        elif userChoice=="5":
-            print("thank you for using BookSwap")
-            break  
-    except :
-        if userChoice!="1"|"2"|"3"|"4"|"5":
-          print("Wrong choice, please reselect ")
+    if userChoice=="1":
+        print("your choice is num 1")
+    elif userChoice=="2":
+        print("your choice is num 2")
+    elif userChoice=="3":
+        print("thank you for using BookSwap app")
+        break
+    else:
+        print(Fore.RED)
+        print("Wrong choice, please reselect")
+            
+   
+        
