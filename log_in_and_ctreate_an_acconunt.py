72750a70d2d44bdcc6c2bfa7779fcdb4a15e1a96
rahaf fallatah <rahaf77553@gmail.com>
2025-02-19 13:15:17 +0300

    Account creation and login file has started to be programmed

--- log_in_and_ctreate_an_acconunt.py
+++ log_in_and_ctreate_an_acconunt.py
@@ -1,4 +1,28 @@
-from art import text2art
 from colorama import Fore, Back, Style
+
 print(Fore.MAGENTA)
-print("welcome to bookswap")
+#لا يمكن للمستخدم الدخول الى البرنامج بدون ان يكون له حساب فيه
+print("welcome to travelmate")
+userChoice1=input("do you want to \n1:Create a new account  2:Sign in\n")
+# بناء على اختيار المستخدم 1 و الذي يمثل انشاء حساب جديد يتم طلب معلوماته لانشاء حساب جديد
+if userChoice1=="1":
+    firstName=input("Enter your first name\n")
+    lastName=input("Enter your last name\n")
+    email=input("Enter your email\n")
+    password=input("Enter your password\n")
+# في حال لم تتطابق كلمة المرور مع تأكيدها لا يتم انشاء الحساب حتى يدخل  المستخدم تأكيد كلمة المرور الصحيح
+    while True:
+        passwordConfirmation=input("Enter your password again for Confirmation\n")
+
+        if password==passwordConfirmation:
+            print("Account created successfully!! Welcome to travelmate {}".format(firstName))
+        else :
+            print(Fore.RED)
+            print("The passwords don't match")
+#بناءا على اختيار المستخدم 2 يتم تسجيل الدخول في البرنامج 
+else: userChoice1=="2"
+print(Fore.MAGENTA)
+firstName=input("Enter your first name\n")
+email=input("Enter your email\n")
+password=input("Enter your password\n")
+print("Welcome back to travelmat {}".format(firstName))
