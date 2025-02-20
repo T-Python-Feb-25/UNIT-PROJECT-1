996002dcdef459d73df85cf42eff85923d661116
rahaf fallatah <rahaf77553@gmail.com>
2025-02-20 14:05:41 +0300

    A library has been added to retrieve weather information and is currently being used in the program.

--- Weather.py
+++ Weather.py
@@ -1,6 +1,19 @@
+'''This file was created to fetch weather 
+information in the city selected by the user and
+ at the time specified by him.'''
+
 import json
+import datetime as dt
 import requests
 
-apiKey="11b1953aab3d44f3824193935251902"
+base_url= "http://api.openweathermap.org/data/2.5/weather?"
+apiKey="2d57597160ec1cc3856f1cafd93ca85f"
+url=base_url+"appid="+apiKey+"&q="+ city
+response= requests.get(url).json
 
+#دالة لجلب معلومات الطقس 
 def get_weather(city, date):
+    
+
+
+
--- food.py
+++ food.py
@@ -1,5 +1,5 @@
 '''
-This file was created to provide the user with the foods that 
+This file was created to provide the user with the restaurants that 
 are famous in the city he has chosen as his travel destination.
 
 '''
