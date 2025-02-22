74a6256b3546b58bf2a790f79e000f100162907a
rahaf fallatah <rahaf77553@gmail.com>
2025-02-22 23:27:13 +0300

    Additional modifications to the main file Modification of the account creation feature

--- main.py
+++ main.py
@@ -13,16 +13,28 @@ answer=input("do you have an account yes or no")
 
 if answer =="no":
      
+     print("Well let's create one for you")
      firstName=input(Fore.MAGENTA+"Enter your first name\n")
      lastName=input(Fore.MAGENTA+"Enter your last name\n")
      while True:
         email=input(Fore.MAGENTA+"Enter your email\n")
+        
+#في حال لم يكن الايميل مثل الفورمات فسيتم الطلب من اليوزر اعادة كتابته
         if not log_in_and_ctreate_an_acconunt.email_format_check(email):
             print (Fore.RED+"the email format is invalid")
+        else:
+            break
 
-        password=input(Fore.MAGENTA+"Enter your password\n")
-# في حال لم تتطابق كلمة المرور مع تأكيدها لا يتم انشاء الحساب حتى يدخل  المستخدم تأكيد كلمة المرور الصحيح
+        while True:
+            password=input(Fore.MAGENTA+"Enter your password\n")
+#في حال كانت كلمة المرور اقل من 8 كارتر لا تقبل 
+            if len(password)<8:
+                print(Fore.RED+"Password must consist of 8 characters")
+            else:
+                break
     
+# في حال لم تتطابق كلمة المرور مع تأكيدها لا يتم انشاء الحساب حتى يدخل  المستخدم تأكيد كلمة المرور الصحيح
+     while True:
         passwordConfirmation=input(Fore.RED+"Confirm your password please \n")
 
         if password==passwordConfirmation:
@@ -42,7 +54,7 @@ elif answer=="yes":
         if len(password)<8:
             print(Fore.RED+"Password must consist of 8 characters")
         else:
-            print(Fore.MAGENTA+"Welcome back to travelmat {}".format(firstName))
+            print(Fore.MAGENTA+"Welcome back to travelmate {}".format(firstName))
             break
                 
             log_in_and_ctreate_an_acconunt.users_login(email, password)
@@ -58,7 +70,7 @@ while True:
     elif userChoice=="2":
         print("your choice is num 2")
     elif userChoice=="3":
-        print("thank you for using ")
+        print("thank you for using travelmate App ")
         break
     else:
         print(Fore.RED)
